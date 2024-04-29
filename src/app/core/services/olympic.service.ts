479313671131c12c4e3b7c71fa5bcf527a29abca
Marconnet Francois <marconnetf@live.fr>
2024-04-29 10:30:15 +0200

    création des modèles de données et suppression des types any

--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -1,26 +1,27 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
+import { Olympic } from '../models/Olympic';
 
 @Injectable({
   providedIn: 'root',
 })
 export class OlympicService {
   private olympicUrl = './assets/mock/olympic.json';
-  private olympics$ = new BehaviorSubject<any>(undefined);
+  private olympics$ = new BehaviorSubject<Olympic[] | null>(null);
 
   constructor(private http: HttpClient) {}
 
-  loadInitialData() {
-    return this.http.get<any>(this.olympicUrl).pipe(
+  loadInitialData() : Observable<Olympic[] | null> {
+    return this.http.get<Olympic[]>(this.olympicUrl).pipe(
       tap((value) => this.olympics$.next(value)),
-      catchError((error, caught) => {
+      catchError((error) => {
         // TODO: improve error handling
         console.error(error);
         // can be useful to end loading state and let the user know something went wrong
         this.olympics$.next(null);
-        return caught;
+        return of(null); 
       })
     );
   }
@@ -29,3 +30,5 @@ export class OlympicService {
     return this.olympics$.asObservable();
   }
 }
+
+
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { Olympic } from 'src/app/core/models/Olympic';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 
 @Component({
@@ -8,11 +9,41 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  public olympics$: Observable<any> = of(null);
+  
+  public olympics$: Observable<Olympic[] | null> = of(null);
 
   constructor(private olympicService: OlympicService) {}
 
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics();
   }
+
+
+  getNumberOfJOs(olympics: Olympic[] | null) : number {
+
+    if (!olympics) {
+      return 0; 
+    }
+
+    const participationId = new Set<number>();
+
+    const result = olympics?.reduce((total, olympic) => {
+      if (olympic.participations) {
+        olympic.participations.forEach((participation) => {
+          if (!participationId.has(participation.id)) {
+            participationId.add(participation.id);
+            total++; 
+          }
+        });
+      }
+      return total;
+    }, 0);
+    
+    return result;
+  }
+
+
+
+  
+
 }
